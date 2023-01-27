f100095b3909d1c5201550daa4cee3303f648e38
Spacepineapple <neal.t.reeves@gmail.com>
2023-01-27 19:00:04 +0000

    Create function to get current conditions in requested location

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ fetch(cityQueryURL)
             .then(response => response.json())
             .then(function(response) {
                 console.log(response);
+                getCurrentConditions(response);
             })
     })
 
+function getCurrentConditions(response) {
+    let temperature = response.list[0].main.temp;
+    let wind = response.list[0].wind.speed;
+    let humidity = response.list[0].main.humidity;
+    console.log(temperature);
+    console.log(wind);
+    console.log(humidity);
+}
