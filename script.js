dd3563ef9a47b3b61ffa319ee6d61e21b9983e0a
Spacepineapple <neal.t.reeves@gmail.com>
2023-01-27 18:23:27 +0000

    Hide API key by storing in local storage

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let lat = "51.52588653564453";
 let lon = "-0.3017457127571106";
-let apiKey = "204554443c50d8fae62b9fffc22f9589";
+let apiKey = localStorage.getItem("apiKey");
+let cityName = "London";
 
+let city = document.querySelector("#search-input").value;
+
+
+//let queryURL = `https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}`
 let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 
